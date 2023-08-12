8fbfa2b1ab2ca662f81406bb61e0bf0ba0e5af5a
Orateng <1818.blackmail@gmail.com>
2023-08-12 13:39:54 -0700

    My filke

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python3
-# The BaseModel script
+"""This script is the base model"""
+
 import uuid
 from datetime import datetime
 from models import storage
-"""The BaseModel class is created"""
 
 
 class BaseModel:
-    """The class BaseModel is made functional"""
+
+    """Class from which all other classes will inherit"""
+
     def __init__(self, *args, **kwargs):
-        # The __init__ function instantiates objects for BaseModel
-        if not kwargs:
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
-        else:
-            tstrf = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], tstrf)
-                if key != '__class__':
-                    setattr(self, key, value)
-
     def __str__(self):
-        # Function that formats string attributes for BaseModel
+        """Returns official string representation"""
+
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        # Function that updates BaseModel from storage
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        Function that creates and  updates dictionay
-        values for BaseModel
-        """
+        """returns a dictionary containing all keys/values of __dict__"""
+
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
