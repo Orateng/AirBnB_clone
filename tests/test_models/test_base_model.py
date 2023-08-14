e2cf94faccbcfac4b88ef8b4765d28e0dba020f0
Orateng <1818.blackmail@gmail.com>
2023-08-13 19:12:12 -0700

    My file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,52 @@
 #!/usr/bin/python3
+"""
+Tests for scrpt base_model
+"""
+import unittest
 from models.base_model import BaseModel
+import datetime
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".
-          format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBase(unittest.TestCase):
+    """Perform unittests for class TestBase"""
+
+    def setUp(self):
+        """The unittest setUp method"""
+        self.my_model = BaseModel()
+
+    def tearDown(self):
+        """The unittest teardown method"""
+        del self.my_model
+
+    def test_id_type(self):
+        """ The test method for id object"""
+        self.assertEqual("<class 'str'>", str(type(self.my_model.id)))
+
+    def test_updated_created(self):
+        """Tests time and date"""
+        self.assertEqual(self.my_model.updated_at.year,
+                         self.my_model.created_at.year)
+
+    def test_to_dict(self):
+        """Tests dict{} method"""
+        self.assertEqual("<class 'dict'>",
+                          str(type(self.my_model.to_dict())))
+
+    def test_to_dict_class(self):
+        """Tests the dict{} class"""
+        self.assertEqual("BaseModel", (self.my_model.to_dict())["__class__"])
+
+    def test_to_dict_updated(self):
+        """Tests format for updated_at method"""
+        self.assertEqual("<class 'str'>",
+                         str(type((self.my_model.to_dict())["updated_at"])))
+
+    def test_to_dict_created(self):
+        """Tests format for create_at method"""
+        tmp = self.my_model.to_dict()
+        self.assertEqual("<class 'str'>", str(type(tmp["created_at"])))
+
+    def test_kwargs(self):
+        """Tests key value object instances"""
+        my_model_dict = self.my_model.to_dict()
+        new_model = BaseModel(**my_model_dict)
+        self.assertEqual(new_model.id, self.my_model.id)
