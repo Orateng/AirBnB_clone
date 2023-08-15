33427204237f2808f80b84d3668b4dca02452523
Orateng <1818.blackmail@gmail.com>
2023-08-14 17:23:14 -0700

    My file

--- tests/test_models/test_save_reload_base_model.py
+++ tests/test_models/test_save_reload_base_model.py
@@ -1,16 +1,78 @@
 #!/usr/bin/python3
-from models import storage
+"""Tests the script file_storage"""
+
+import os
+import time
+import json
+import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+from datetime import datetime
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+class Test_FileStorage(unittest.TestCase):
+    """Testcases for the class FileStorage"""
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+    def setUp(self):
+        """Test setUp method for class models"""
+        self.storage = FileStorage()
+        self.my_model = BaseModel()
+
+    def tearDown(self):
+        """Test tearDown method for class Filetorage"""
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_all(self):
+        """Test all function attributes"""
+        storage_all = self.storage.all()
+        self.assertIsInstance(storage_all, dict)
+
+    def test_new_method(self):
+        """Tests object values for new method"""
+        self.storage.new(self.my_model)
+        key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
+        self.assertTrue(key in self.storage._FileStorage__objects)
+
+    def test_objects(self):
+        """Tests class objects"""
+        self.storage.new(self.my_model)
+        key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
+        val = self.storage._FileStorage__objects[key]
+        self.assertIsInstance(self.my_model, type(val))
+
+    def test_save_file_exists(self):
+        """Tests input files created"""
+        self.storage.save()
+        self.assertTrue(os.path.isfile("file.json"))
+
+    def test_save_file_read(self):
+        """Tests conctents of saved file"""
+        self.storage.save()
+        self.storage.new(self.my_model)
+
+        with open("file.json", encoding="UTF8") as f:
+            content = json.load(f)
+
+        self.assertTrue(type(content) is dict)
+
+    def test_files_saved(self):
+        """Tests the contnts inside the file"""
+        self.storage.save()
+        self.storage.new(self.my_model)
+
+        with open("file.json", encoding="UTF8") as f:
+            content = f.read()
+
+        self.assertIsInstance(content, str)
+
+    def test_reload_save(self):
+        """Tests saved files when created"""
+        try:
+            self.storage.reload()
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+
